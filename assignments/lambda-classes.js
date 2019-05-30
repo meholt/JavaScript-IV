e3132c4e76e93b2d5cfc1dd222c6701843faaccd
meholt <meholt90@gmail.com>
2019-05-29 21:49:53 -0700

    finished-prototype-section

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,21 @@
 // CODE here for your Lambda Classes
+
+class Person {
+    constructor(attributes) {
+        this.name = attributes.name;
+        this.age = attributes.age;
+        this.location = attributes.location;
+    }
+    speak() {
+        return `Hello, my name is ${this.name}, I am from ${this.location}`;
+    }
+}
+
+class Instructor {
+    constructor(attributes_instructor) {
+        super(attributes_instructor);
+        this.specialty = attributes_instructor.specialty;
+        this.favLanguage = attributes_instructor.favLanguage;
+        this.catchPhrase = attributes_instructor.catchPhrase;
+    }
+}
